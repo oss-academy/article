ac3eaaf15fe378514de899f0805ad8f1d10cd405
Saman Alishirishahrbabak <samanalishiri@gmail.com>
2022-12-21 22:09:19 +0100

    temp

--- mvc/mvc-sample/src/main/java/com/tutorial/article/view/ConsoleView.java
+++ mvc/mvc-sample/src/main/java/com/tutorial/article/view/ConsoleView.java
@@ -2,18 +2,23 @@ package com.tutorial.article.view;
 
 import com.tutorial.article.model.Model;
 
-import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Stream;
 
 import static java.lang.String.format;
 import static java.util.logging.Logger.getLogger;
+import static java.util.stream.Collectors.joining;
 
 public final class ConsoleView implements View<Model> {
 
     private final Logger logger = getLogger(ConsoleView.class.getSimpleName());
 
     private Model[] models = new Model[0];
+
+    public ConsoleView() {
+        logger.setLevel(Level.ALL);
+    }
 
     @Override
     public Model[] getModels() {
@@ -27,8 +32,16 @@ public final class ConsoleView implements View<Model> {
 
     @Override
     public void view() {
-        if (logger.isLoggable(Level.ALL)) {
-            logger.info(format("%n %s %n", Arrays.toString(models)));
+        if (logger.isLoggable(Level.INFO)) {
+            var message = "Models" +
+                    "\n" +
+                    "=================================================" +
+                    "\n\n" +
+                    format("%s", Stream.of(models).map(Model::toString).collect(joining("\n"))) +
+                    "\n\n" +
+                    "=================================================" +
+                    "\n";
+            logger.info(message);
         }
     }
 
